4049fe00bdd33feab85e75fee932c20d20841918
FRANCIS MILES BASCONES ANG <2202545@cc.usls.edu>
2025-02-12 09:27:15 +0800

    Feat: Creating item add form

---
+++ resources/views/inventory/index.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Bootstrap demo</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <div class="container">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Item Name</th>
+            <th scope="col">Price</th>
+            <th scope="col">Stock</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1000</th>
+            <td>Pencil</td>
+            <td>5</td>
+            <td>1005</td>
+          </tr>
+          <tr>
+            <th scope="row">1001</th>
+            <td>Eraser</td>
+            <td>5</td>
+            <td>1010</td>
+          </tr>
+          <tr>
+            <th scope="row">1002</th>
+            <td>Paper</td>
+            <td>1</td>
+            <td>1015</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,8 @@ Route::get('/', function () {
 
 Route::get('/login', 'App\Http\Controllers\LoginController@index');
 
+Route::get('/inventory', 'App\Http\Controllers\InventoryController@index');
+
+Route::get('/create', 'App\Http\Controllers\InventoryController@create');
+
 Route::get('/hello', 'App\Http\Controllers\HelloController@index');
