008fd8f19a953176861dcb9f60df2bb66e2d7ef5
LuciYin <s2202545@usls.edu.ph>
2025-02-17 23:19:46 +0800

    Feat: Making page responsive and Integrating Bootstrap

--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -13,36 +13,38 @@
   </head>
   <body>
     <div class="container">
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Item Name</th>
-            <th scope="col">Price</th>
-            <th scope="col">Stock</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1000</th>
-            <td>Pencil</td>
-            <td>5</td>
-            <td>1005</td>
-          </tr>
-          <tr>
-            <th scope="row">1001</th>
-            <td>Eraser</td>
-            <td>5</td>
-            <td>1010</td>
-          </tr>
-          <tr>
-            <th scope="row">1002</th>
-            <td>Paper</td>
-            <td>1</td>
-            <td>1015</td>
-          </tr>
-        </tbody>
-      </table>
+      <div class="table-responsive">
+        <table class="table table-striped">
+          <thead class="table-dark">
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Item Name</th>
+              <th scope="col">Price</th>
+              <th scope="col">Stock</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1000</th>
+              <td>Pencil</td>
+              <td>5</td>
+              <td>1005</td>
+            </tr>
+            <tr>
+              <th scope="row">1001</th>
+              <td>Eraser</td>
+              <td>5</td>
+              <td>1010</td>
+            </tr>
+            <tr>
+              <th scope="row">1002</th>
+              <td>Paper</td>
+              <td>1</td>
+              <td>1015</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
     <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -10,32 +10,38 @@
       integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
       crossorigin="anonymous"
     />
+    <style>
+      @media (max-width: 576px) {
+        .fixed-width {
+          width: 576px;
+        }
+      }
+
+      /* Not me setting a max width because ga very long (vertically) if I don't haha */
+
+    </style>
   </head>
   <body>
-    <div class="container">
-      <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label"
-          >Email address</label
-        >
-        <input
-          type="email"
-          class="form-control"
-          id="exampleFormControlInput1"
-          placeholder="name@example.com"
-        />
-        <label for="inputPassword5" class="form-label">Password</label>
-        <input
-          type="password"
-          id="inputPassword5"
-          class="form-control"
-          aria-describedby="passwordHelpBlock"
-        />
-        <div id="passwordHelpBlock" class="form-text">
-          Your password must be 8-20 characters long, contain letters and
-          numbers, and must not contain spaces, special characters, or emoji.
-        </div>
+    <div class="container mt-5">
+      <div class="row justify-content-center">
+          <div class="col-9 col-sm-9 col-md-10 col-lg-11 col-xl-12 fixed-width">
+              <div class="card p-4 shadow">
+                  <h3 class="text-center">Login</h3>
+                  <div class="mb-3">
+                      <label for="email" class="form-label">Email address</label>
+                      <input type="email" class="form-control" id="email" placeholder="name@example.com" />
+                  </div>
+                  <div class="mb-3">
+                      <label for="password" class="form-label">Password</label>
+                      <input type="password" id="password" class="form-control" aria-describedby="passwordHelpBlock" />
+                      <div id="passwordHelpBlock" class="form-text">
+                          Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                      </div>
+                  </div>
+                  <button class="btn btn-primary w-100">Login</button>
+              </div>
+          </div>
       </div>
-    </div>
     <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
