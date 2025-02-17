ef02c2f33940e9d3345517c6d2c6a35fa01e22ec
LuciYin <s2202545@usls.edu.ph>
2025-02-17 23:58:33 +0800

    Feat: Create categories and items tables migration.

--- 2025_02_17_153458_create_items_table.php
+++ 2025_02_17_153458_create_items_table.php
@@ -4,9 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateItemsTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id(); 
@@ -23,8 +26,11 @@ class CreateItemsTable extends Migration
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('items');
     }
-}
+};
